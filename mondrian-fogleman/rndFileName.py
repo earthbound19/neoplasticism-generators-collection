d7b717508997827f70abdf283739b7febf24b60c
Alex Hall <ansible1@earthbound.io>
2018-05-07 08:20:01 -0600

    dev snapshot

--- mondrian-fogleman/rndFileName.py
+++ mondrian-fogleman/rndFileName.py
@@ -3,7 +3,7 @@ import random
 # Function to generate random file names for generated images:
 def random_string(length):
     charSet = '23456789abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ'
-    return ''.join(random.choice(charSet) for i in range(length))
+    return 'MFH_' + ''.join(random.choice(charSet) for i in range(length)) + '.hexplt'
 
-fileNameStr = 'Mondrian-Fogleman-Hall_' + random_string(15)
+fileNameStr = random_string(15)
 print(fileNameStr)
