d90179f44201b9af2e397f9bc79cb414aaaebf03
earthbound19 <ansible1@earthbound.io>
2018-05-09 15:43:02 +0100

    deleted (part of piet.py now)

--- mondrian-fogleman/rndFileName.py
+++
@@ -1,9 +0,0 @@
-import random
-
-# Function to generate random file names for generated images:
-def random_string(length):
-    charSet = '23456789abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ'
-    return 'MFH_' + ''.join(random.choice(charSet) for i in range(length)) + '.hexplt'
-
-fileNameStr = random_string(15)
-print(fileNameStr)
