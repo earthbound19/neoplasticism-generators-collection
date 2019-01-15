30f4837911dfc8a1054158ca22a20a4a10336d44
Alex Hall <ansible1@earthbound.io>
2019-01-14 22:10:51 -0700

    add more svg output examples

--- mondrian-fogleman-hill-hall/example1.py
+++ mondrian-fogleman-hill-hall/example1.py
@@ -2,4 +2,4 @@ from mondrian import Mondrian
 
 canvas = Mondrian()
 canvas.make_painting()
-canvas.write_svg('example1.svg')
+canvas.write_svg('example1c.svg')
--- mondrian-fogleman-hill-hall/example2.py
+++ mondrian-fogleman-hill-hall/example2.py
@@ -5,4 +5,4 @@ width, height = 600, 450
 minarea = 5000 / (width * height)
 canvas = Canvas(width, height)
 canvas.make_painting(nlines, minarea, orthogonal=False)
-canvas.write_svg('example2.svg')
+canvas.write_svg('example2b.svg')
